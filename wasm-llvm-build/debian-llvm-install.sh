a605b81b0b05175e4dde3990ce0263217f378c2c
Kees van Dieren <keesvandieren@squins.com>
2020-11-26 12:07:28 +0100

    #14 Do not use ubuntu 16.04 repo, try 18.04 (bionic)

--- wasm-llvm-build/debian-llvm-install.sh
+++ wasm-llvm-build/debian-llvm-install.sh
@@ -6,14 +6,22 @@
 # exit on first error
 set -e
 
-sudo apt-get update -y
-sudo apt-get install -y llvm
+fatal () { echo "fatal: $*"; exit 1; }
+logged () { echo "+ $*"; eval "$*"; }
+cmd () { logged "$*" || fatal "command failed: $*"; }
+
+cmd sudo apt-get update -y
+cmd sudo apt-get install -y llvm
 #sudo chmod +x .travis/deploy.sh
-sudo wget https://apt.llvm.org/llvm-snapshot.gpg.key
-sudo apt-key add llvm-snapshot.gpg.key
-sudo add-apt-repository -y "deb http://apt.llvm.org/xenial/   llvm-toolchain-xenial-10  main"
-sudo apt-get -q update
-sudo apt-get install -y clang-10 lldb-10 lld-10 clangd-10
+cmd sudo wget https://apt.llvm.org/llvm-snapshot.gpg.key
+cmd sudo apt-key add llvm-snapshot.gpg.key
+cmd sudo add-apt-repository -y "deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic-10  main"
+cmd sudo apt-get -q update
+cmd sudo apt-get install -y clang-10
+cmd sudo apt-get install -y lldb-10
+cmd sudo apt-get install -y lld-10
+cmd sudo apt-get install -y clangd-10
+
 #wget https://github.com/gohugoio/hugo/releases/download/v0.59.0/hugo_0.59.0_Linux-64bit.tar.gz
 #tar xzf hugo_0.59.0_Linux-64bit.tar.gz
 #sudo chmod +x hugo
