55e64271c303ee7aa2995e66a177e525a1c6a15b
Coen <coenrijsdijk@gmail.com>
2020-12-07 23:41:55 +0100

    shaded ClassReflection and test new method

---
+++ gdx-backend-bytecoder/src/test/kotlin/com/badlogic/gdx/utils/reflect/ClassReflectionTest.kt
@@ -0,0 +1,27 @@
+package com.badlogic.gdx.utils.reflect
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ClassReflectionTest {
+//     val listOfExampleClasses = mutableListOf("BitmapFont" ,"Color", "Skin.TintedDrawable")
+    val exceptedClasses = mutableListOf<String>("Bitmap", "Color", "Skin.TintedDrawable")
+
+    val listOfExampleClasses = mutableListOf<Class<*>>(BitmapFont::class.java, Color::class.java, Skin.TintedDrawable::class.java)
+
+    @Test
+    internal fun getSimpleName() {
+        val listOfSimpleNameClasses : MutableList<String> = mutableListOf()
+        for (i in listOfExampleClasses.withIndex()){
+            listOfSimpleNameClasses.add(i.value.simpleName)
+        }
+
+
+
+        assertEquals(exceptedClasses, listOfSimpleNameClasses)
+    }
+}
