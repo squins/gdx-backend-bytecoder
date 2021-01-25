5778485690980454526833ba19c758a02df02ec1
CoenRijsdijk <coenrijsdijk@gmail.com>
2021-01-25 08:25:42 +0100

    #6 add more tests

--- gdx-backend-bytecoder/src/test/kotlin/com/squins/gdx/backends/bytecoder/preloader/PreloaderTest.kt
+++ gdx-backend-bytecoder/src/test/kotlin/com/squins/gdx/backends/bytecoder/preloader/PreloaderTest.kt
@@ -7,8 +7,6 @@ class PreloaderTest {
 
     @Test
     fun convertAssetsWorksAsExpected() {
-
-
         val assets = Preloader.convertToAssets("""
                 i:badlogic.jpg:68465:image/jpeg:1
                 a:sample.mp3:646974:audio/mp3:0""".trimIndent())
@@ -18,7 +16,22 @@ class PreloaderTest {
                         Asset("badlogic.jpg",  AssetType.Image, 68465, "image/jpeg", true),
                         Asset("sample.mp3", AssetType.Audio,646974, "audio/mp3", false )
                 ), assets)
-
     }
 
+    @Test
+    fun convertAssetsFailsAsExpected() {
+        val assets = Preloader.convertToAssets("""
+                i:sample.jpg:68465:image/jpeg:1
+                a:sample.mp3:646974:audio/mp3:0""".trimIndent())
+
+        assertNotEquals(listOf(
+                Asset("sample.jpg", AssetType.Audio, 68465, "image/jpeg", true),
+                Asset("sample.mp3", AssetType.Image, 646974, "audio/mp3", false)
+        ), assets)
+
+        assertNotEquals(listOf(
+                Asset("sample.jpg", AssetType.Image, 68465, "sample.mp3", true),
+                Asset("sample.mp3", AssetType.Audio, 646974, "audio/mp3", false)
+        ), assets)
+    }
 }
