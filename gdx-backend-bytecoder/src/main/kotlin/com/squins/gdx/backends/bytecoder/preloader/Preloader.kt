c91c246d0b4b3e9dfd470428e68ab8b9a754fc21
Kees van Dieren <keesvandieren@squins.com>
2021-06-26 23:45:09 +0200

    Fix warning

--- gdx-backend-bytecoder/src/main/kotlin/com/squins/gdx/backends/bytecoder/preloader/Preloader.kt
+++ gdx-backend-bytecoder/src/main/kotlin/com/squins/gdx/backends/bytecoder/preloader/Preloader.kt
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Files
 import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.utils.ObjectMap
 import com.squins.gdx.backends.bytecoder.files.BytecoderFileHandle
-import com.squins.gdx.backends.bytecoder.api.web.HTMLAudioElement
 import com.squins.gdx.backends.bytecoder.api.web.HTMLImageElement
 import com.squins.gdx.backends.bytecoder.makeAndLogIllegalArgumentException
 import de.mirkosertic.bytecoder.api.web.Window
